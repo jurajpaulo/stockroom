aecbee66a08985d6b8609a660d47938fa541f177
Juraj Paulo <jurajpaulo@yahoo.com>
2012-11-23 21:21:44 +0100

    adding first user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121123230347) do
+ActiveRecord::Schema.define(:version => 20121123230348) do
 
   create_table "session_events", :force => true do |t|
     t.integer  "user_id",    :null => false
